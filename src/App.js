3d698cdd0a0bf9446f9d62f08ae60ba95d197898
Mihail <mihail.treschalin@ya.ru>
2020-05-19 16:24:42 +0300

    Выполнил задание из урока - Что такое JSX?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const str1 = 'Hello!';
+const str2 = 'Bye!';
+const condition = true;
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +14,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <ul style={{textAlign: 'left'}}>
+          <li>вывод числа: {3}</li>
+          <li>вывод результата арифметической операции: {48 / 2}</li>
+          <li>вывод переменной и результата логической операции: {true && str1}</li>
+          <li>вывод результата тернарного условия: {!condition ? str1 : str2}</li>
+          <li>вывод undefined: -{undefined}, null: -{null}, false: -{false}, true:  -{true}</li>
+        </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
