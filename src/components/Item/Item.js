f9e5cfb994f8cb958438ed6950c88835dc138c2c
MadMike80 <mihail.treschalin@ya.ru>
2020-10-29 19:58:59 +0300

    leak memory

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -9,15 +9,14 @@ import styles from './Item.module.css';
 class Item extends React.Component {
   
   componentDidMount() {
-    console.log('componentDidMount');
+    this.idInterval = setInterval(() => { 
+      console.log('leak memory');
+    }, 1000);
   }
   
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-  
+
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    clearInterval(this.idInterval);
   }
   
   render () {
