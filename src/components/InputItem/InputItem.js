2d368d8729a720deddc5744f3a7515f5ffa84a11
MadMike80 <mihail.treschalin@ya.ru>
2020-11-17 17:33:12 +0300

    Converted class components into functional ones, and used hooks

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,62 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import styles from './InputItem.module.css';
 
-class InputItem extends React.Component {
-  state = {
+const InputItem = ({onClickAdd}) => {
+  const inputState = {
     inputValue: '',
     error: false,
     helperText: ''
   };
-
-  onAddClick = () => {
-    if (this.state.inputValue.length > 0) {
-      this.setState(
-        {inputValue: ''}
-      );
-      this.props.onClickAdd(this.state.inputValue);
+  
+  const [inputValue, setInputValue] = useState(inputState.inputValue);
+  const [error, setError] = useState(inputState.error);
+  const [helperText, setHelperText] = useState(inputState.helperText);
+  
+  const onAddClick = () => {
+    if (inputValue.length > 0) {
+      const newInputValue = '';
+      setInputValue(newInputValue);
+      onClickAdd(inputValue);
     } else {
-      this.setState(
-        {
-          helperText: 'Поле не должно быть пустым!',
-          error: true
+      const newHelperText = 'Поле не должно быть пустым!';
+      const newError = true;
+      setHelperText(newHelperText);
+      setError(newError);
+      }
+  };
+  
+  return (
+    <form className={styles.inputWrap}>
+      <TextField
+        className={styles.inputField}
+        label='Введите новое дело'
+        id='outlined-margin-none'
+        variant='outlined'
+        error={error}
+        helperText={helperText}
+        multiline
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value.toUpperCase());
+          setHelperText('');
+          setError(false);
+          }
         }
-      );
-    }
-  }
-
-  render() {
-
-    return (
-      <form className={styles.inputWrap}>
-        <TextField
-          className={styles.inputField}
-          label='Введите новое дело'
-          id='outlined-margin-none'
-          variant='outlined'
-          error={this.state.error}
-          helperText={this.state.helperText}
-          multiline
-          value={this.state.inputValue}
-          onChange={(event) => this.setState({
-            inputValue: event.target.value.toUpperCase(),
-            helperText: '',
-            error: false
-          })}
-        />
-        <Fab
-          size='medium'
-          color='primary'
-          aria-label='add'
-          onClick={this.onAddClick}
-        >
-          <AddIcon />
-        </Fab>
-      </form>
-    );
-  }
+      />
+      <Fab
+        size='medium'
+        color='primary'
+        aria-label='add'
+        onClick={onAddClick}
+      >
+        <AddIcon />
+      </Fab>
+    </form>
+  );
 }
 
 export default InputItem;
