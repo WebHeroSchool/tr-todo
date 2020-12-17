c4fd057113bae112ee5a3117452cc0c85291a02a
MadMike80 <mihail.treschalin@ya.ru>
2020-12-18 00:41:41 +0300

    Added preloader in About component.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Octokit } from '@octokit/rest';
 
-const About = () => (
-	<h1>Обо мне</h1>
-);
+const octokit = new Octokit();
+class About extends React.Component {
+	state = {
+		isLoading: true,
+		repoList: [],
+	}
+	
+	componentDidMount() {
+		octokit.repos.listForUser({
+			username: 'stasokulov',
+		}).then(({data}) => console.log(data));
+	}
+	
+	render() {
+		const {isLoading} = this.state;
+		
+		return (
+			<h1>{isLoading ? <CircularProgress color="secondary" /> : 'Обо мне'}</h1>
+		)
+	}
+}
 
 export default About;
