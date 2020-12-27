3b382c99eb0d608dc1b5be725c8c8b2cc5285eeb
MadMike80 <mihail.treschalin@ya.ru>
2020-12-28 00:48:18 +0300

    Added handling of erroneous requests. Added styles.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,52 +1,80 @@
 import React from 'react';
 
+import styles from './About.module.css';
+import classnames from 'classnames';
+
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Octokit } from '@octokit/rest';
 
 const octokit = new Octokit();
 class About extends React.Component {
 	state = {
+		username: 'MadMike80',
 		isLoading: true,
 		repoList: [],
 		userInfo: [],
+		incorrectRequest: false,
 	}
 	
 	componentDidMount() {
 
+		const {username} = this.state;
+
 		octokit.repos.listForUser({
-			username: 'MadMike80',
+			username,
 		}).then(({data}) => {
 			this.setState({
 				isLoading: false,
 				repoList: data,
 			})
-			// console.log(data);
+		}).catch(() => {
+			this.setState({
+				incorrectRequest: true,
+			})
 		});
 
 		octokit.users.getByUsername({
-			username: 'MadMike80',
+			username,
 		}).then(({data}) => {
 			this.setState({
 				isLoading: false,
 				userInfo: data,
 			})
-			console.log(data);
+		}).catch(() => {
+			this.setState({
+				incorrectRequest: true,
+			})
 		});
 
 	}
 	
 	render() {
-		const { isLoading, repoList } = this.state;
+		const { isLoading, repoList, userInfo, incorrectRequest } = this.state;
 		
 		return (
-			<div>
-				<h1>{isLoading ? <CircularProgress color="secondary" /> : 'Обо мне'}</h1>
-				<ul>
-					{ repoList.map( repo => (
-						<li key={repo.id}>
-							<a href={repo.html_url}>{repo.name}</a>
-						</li>))}
-				</ul>
+			<div
+				className={classnames({
+					[styles.wrap]: true,
+					[styles.error]: isLoading,
+				})}
+			>
+				{isLoading ? <CircularProgress color="secondary" /> : <div>
+					<h1>Обо мне</h1>
+					<div className={styles.img__wrap}>
+						<img className={styles.avatar} src={userInfo.avatar_url} alt='Avatar'/>
+						<div className={styles.login}> Login to GitHub: <span className={styles.text}>{userInfo.login}</span></div>
+					</div>
+					<p> My name is <span className={styles.text}>{userInfo.name}</span></p>
+					<p> {userInfo.bio}</p>
+					<p>My repositories:</p>
+					<ol className={styles.list}>
+						{ repoList.map( repo => (
+							<li className={styles.item} key={repo.id}>
+								<a className={styles.link} href={repo.html_url}>{repo.name}</a>
+							</li>))}
+					</ol></div>
+				}
+				{incorrectRequest && <div>Ошибка запроса!</div>}
 			</div>
 		)
 	}
